d2ded5aad925afe05579768a6f026b7a91bf6cc9
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2024-12-10 15:23:59 +0200

    Add files via upload

---
+++ SCRIMBA LEARN TO CODE/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('toggle-btn').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px';
+    }
+});
